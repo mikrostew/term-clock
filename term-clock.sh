0ee0659cda605a9ee8a7c7cb777d8b126246312b
Michael Stewart <mistewart@linkedin.com>
2019-05-23 23:10:15 -0700

    only update the clock in the active iTerm tab

--- term-clock.sh
+++ term-clock.sh
@@ -1,18 +1,49 @@
 #!/usr/bin/env bash
 
+platform="$(uname)"
+
+save_cursor='\e[s'
+restore_cursor='\e[u'
+move_cursor_str='\e[1;%dH' # placeholder for offset
+
+# TODO: help/usage text
+
+# see https://www.iterm2.com/documentation-scripting.html
+active_iterm_tab_id() {
+  if [ "$platform" == "Darwin" ]
+  then
+    osascript <<EOF
+    tell application "iTerm2"
+      tell current session of current window
+        id
+      end tell
+    end tell
+EOF
+  fi
+}
+
 # put a clock in the top right corner
-# TODO: split this off into it's own repo
-# (adapted from https://www.commandlinefu.com/commands/view/7916/put-a-console-clock-in-top-right-corner and https://stackoverflow.com/a/18773677/)
+# adapted from:
+# - https://www.commandlinefu.com/commands/view/7916/put-a-console-clock-in-top-right-corner
+# - https://stackoverflow.com/a/18773677/
+#
 # this updates the time every 5 seconds (since it only shows minute precision)
 terminal_clock() {
   # this will ignore SIGINT (Ctrl-C) here, so sending that to the parent shell does not close this
   trap '' SIGINT
 
+  # TODO: pass this to the script as an argument (or does the script get this automatically...)
+  if [ -n "$ITERM_SESSION_ID" ] && [[ "$ITERM_SESSION_ID" =~ :([0-9A-F-]*) ]]
+  then
+    iterm_id="${BASH_REMATCH[1]}"
+  fi
+  echo "iTerm ID: $iterm_id"
+
+  # TODO: this will have to be an argument
   shell_pid="$$"
   echo "shell PID: $shell_pid"
-  save_cursor='\e[s'
-  restore_cursor='\e[u'
-  move_cursor_str='\e[1;%dH' # placeholder for offset
+  # TODO: and this should exit when that PID goes away (session is closed)
+
   # update every 5 seconds (knowing the time all the time is not that important)
   # TODO: make this value configurable
   while sleep 5
@@ -24,10 +55,14 @@ terminal_clock() {
     #  comm shows the command (but not the arguments, so this doesn't match the grep)
     # this greps for the shell PID, filters out the bash process running the clock, and looks for any FG process
     fg_proc="$(ps -e -o state= -o ppid= -o comm= | grep -Fw $shell_pid | grep -v "bash" | grep -F '+')"
-    if [ -z "$fg_proc" ]
+
+    # and only update the clock if this iTerm tab is the active one
+
+    if [ -z "$fg_proc" ] && [[ -z "$iterm_id" || "$iterm_id" == "$(active_iterm_tab_id)" ]]
     then
       # need to do these calculations every time
       # TODO: calculate the length of the curr_datetime string, and use that here instead of a magic number
+      # TODO: also, if this is wider than the terminal width, don't display it
       col_offset="$(( $(tput cols)-22 ))"
       curr_datetime="$(date +'%a %b %d, %H:%M %Z')" # formatted like "Mon May 20, 14:36 PDT"
       move_cursor="$(printf "$move_cursor_str" "$col_offset")"
@@ -37,4 +72,3 @@ terminal_clock() {
     fi
   done
 }
-
